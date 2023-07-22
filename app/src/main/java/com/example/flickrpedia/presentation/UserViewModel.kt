da223b0d89bf1ff221aa8b46e5dfca758831ba64
Mustafa Khaled <m.khaled@rewe-group.at>
2023-07-22 14:54:52 +0200

    change toolbar text color and background

--- app/src/main/java/com/example/flickrpedia/presentation/UserViewModel.kt
+++ app/src/main/java/com/example/flickrpedia/presentation/UserViewModel.kt
@@ -3,20 +3,53 @@ package com.example.flickrpedia.presentation
 import android.app.Application
 import android.util.Patterns
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.flickrpedia.R
+import com.example.flickrpedia.data.model.UserModel
+import com.example.flickrpedia.domain.repo.LoginUser
+import com.example.flickrpedia.domain.repo.RegisterUser
+import com.example.flickrpedia.misc.State
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel @Inject constructor(private val application: Application) :
+class UserViewModel @Inject constructor(private val application: Application, private val registerUser: RegisterUser, private val loginUser: LoginUser) :
     AndroidViewModel(application) {
+    private val _uiState = MutableStateFlow<UserUiState?>(null)
+    val uiState = _uiState.asStateFlow()
+
     private val _userType = MutableStateFlow(UserType.LOGIN)
     val userType = _userType.asStateFlow()
+
+    fun register(email: String, password: String, age:String) {
+        viewModelScope.launch {
+            _uiState.value = UserUiState.Loading
+            delay(3000) // Fake Loading
+            val result = registerUser.register(email,password,age)
+            _uiState.value = when(result) {
+                is State.Error -> UserUiState.ErrorRegister(result.msg.toString())
+                is State.Loading ->  { UserUiState.Loading }
+                is State.Success -> UserUiState.SuccessRegistration(result.data)
+            }
+        }
+    }
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            _uiState.value = UserUiState.Loading
+            delay(3000) // Fake Loading
+            val result = loginUser.login(email,password)
+            _uiState.value = when(result) {
+                is State.Error -> UserUiState.ErrorLogin(result.msg.toString())
+                is State.Loading ->  { UserUiState.Loading }
+                is State.Success -> UserUiState.SuccessLogin(result.data.email)
+            }
+        }
+    }
 
     fun changeUserType(userType: UserType) {
         _userType.value = userType
@@ -51,6 +84,13 @@ class UserViewModel @Inject constructor(private val application: Application) :
             Pair(true, "")
     }
 
+    sealed class UserUiState {
+        object Loading : UserUiState()
+        data class SuccessRegistration(val userModel: UserModel): UserUiState()
+        data class SuccessLogin(val email: String): UserUiState()
+        data class ErrorRegister(val message: String): UserUiState()
+        data class ErrorLogin(val message: String): UserUiState()
+    }
     enum class UserType {
         LOGIN, REGISTER
     }
