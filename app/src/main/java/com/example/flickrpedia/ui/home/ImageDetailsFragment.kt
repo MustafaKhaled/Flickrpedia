1de73e2fee213e7b1be6e04ca5c967c5178a02bb
Mustafa Khaled <m.khaled@rewe-group.at>
2023-07-22 21:45:07 +0200

    added navigation icon added section for image details

--- app/src/main/java/com/example/flickrpedia/ui/home/ImageDetailsFragment.kt
+++ app/src/main/java/com/example/flickrpedia/ui/home/ImageDetailsFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.flickrpedia.R
 import com.example.flickrpedia.databinding.FragmentImageDetailsBinding
 
 class ImageDetailsFragment: Fragment() {
@@ -23,6 +26,20 @@ class ImageDetailsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
+        imageSectionBind()
+    }
 
+    private fun imageSectionBind() {
+        Glide.with(binding.root).load(args.hit.largeImageUrl).into(binding.image)
+
+        binding.imageSize.label.text = getString(R.string.image_size_label)
+        binding.imageSize.value.text = args.hit.imageSize.toString()
+
+        binding.imageType.label.text = getString(R.string.image_type_label)
+        binding.imageType.value.text = args.hit.type
+
+        binding.imageTags.label.text = getString(R.string.image_type_label)
+        binding.imageTags.value.text = args.hit.tags
     }
 }
